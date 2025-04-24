da91b32ab0eb7d1e3faf95e22b9a3ef6772dd28d
Dmitry Dorogoy <Dmitrii_Dorogoi@epam.com>
2025-04-24 13:10:48 +0200

    WIP

--- ChatClient.Api/ApiExceptionFilter.cs
+++ ChatClient.Api/ApiExceptionFilter.cs
@@ -3,18 +3,11 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace ChatClient.Api;
 
-public class ApiExceptionFilter : IAsyncExceptionFilter
+public class ApiExceptionFilter(ILogger<ApiExceptionFilter> logger) : IAsyncExceptionFilter
 {
-    private readonly ILogger<ApiExceptionFilter> _logger;
-
-    public ApiExceptionFilter(ILogger<ApiExceptionFilter> logger)
-    {
-        _logger = logger;
-    }
-
     public Task OnExceptionAsync(ExceptionContext context)
     {
-        _logger.LogError(context.Exception, "Unhandled exception occurred during request.");
+        logger.LogError(context.Exception, "Unhandled exception occurred during request.");
 
         context.Result = new ObjectResult(new { error = "An internal error occurred." })
         {
