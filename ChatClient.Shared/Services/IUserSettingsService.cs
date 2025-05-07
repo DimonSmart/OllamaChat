1ee5ed127c8176e5101dd1c4ecdaae0c4b7ce447
Dmitrii Dorogoi <Dmitrii_Dorogoi@epam.com>
2025-05-07 16:53:09 +0200

    Introduced IUserSettingsService

---
+++ ChatClient.Shared/Services/IUserSettingsService.cs
@@ -0,0 +1,16 @@
+using ChatClient.Shared.Models;
+
+namespace ChatClient.Shared.Services;
+
+public interface IUserSettingsService
+{
+    /// <summary>
+    /// Gets the current user settings
+    /// </summary>
+    Task<UserSettings> GetSettingsAsync();
+    
+    /// <summary>
+    /// Saves the user settings
+    /// </summary>
+    Task SaveSettingsAsync(UserSettings settings);
+}
