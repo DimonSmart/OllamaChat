dc59fa57467c360c5f07ce435513d3d5b3c79bb0
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2025-08-12 14:37:56 +0200

    Remove unused Agent checkbox and related settings (#82)

--- ChatClient.Shared/Models/ChatConfiguration.cs
+++ ChatClient.Shared/Models/ChatConfiguration.cs
@@ -3,7 +3,6 @@ namespace ChatClient.Shared.Models;
 public record ChatConfiguration(
     string ModelName,
     IReadOnlyCollection<string> Functions,
-    bool UseAgentResponses,
     int MaximumInvocationCount = 1,
     string StopAgentName = "",
     string StopPhrase = "");
